fd99ba4f4566e52118bbed2f176e89e4784f500c
Venkateshgujrati <gujrativenkatesh@gmail.com>
2022-06-20 13:43:55 +0530

    python project files

---
+++ max.py
@@ -0,0 +1,13 @@
+a=int(input("enter a value:-"));
+b=int(input("enter a value:-"));
+c=int(input("enter a value:-"));
+
+
+if a>b and a>c:
+    print("a");
+
+elif b>a and b>c:
+   print("b");  
+
+else:
+    print("c")       
